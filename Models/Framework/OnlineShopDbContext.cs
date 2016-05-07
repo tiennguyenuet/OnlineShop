143221fe43c3ebb4d6784ea166ef1a565c968681
= <tiennguyenuet@gmail.com>
2016-05-07 22:21:36 +0700

    OnlineShop Created

---
+++ Models/Framework/OnlineShopDbContext.cs
@@ -0,0 +1,21 @@
+namespace Models.Framework
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class OnlineShopDbContext : DbContext
+    {
+        public OnlineShopDbContext()
+            : base("name=OnlineShopDbContext")
+        {
+        }
+
+        public virtual DbSet<Account> Accounts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ OnlineShop/Areas/Admin/Models/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.Areas.Admin.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string UserName { set; get; }
+
+        [Required]
+        public string Password { set; get; }
+
+        public bool RememberMe { set; get; }
+
+    }
+}
